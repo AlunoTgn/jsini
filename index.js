e2fc714013051e91dd17e854b641352017277b1f
AlunoTgn <carlosgabreuneto@gmail.com>
2020-08-20 20:26:49 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,16 @@
+var x = 6;
+
+console.log(x);
+
+const y = "a";//definição de variável const
+//y = "b";
+console.log(y);
+//VARIÁVEL LOCAIS E GLOBAIS
+{//bloco de códigos
+  let z = 4;
+  console.log(x);
+  console.log(y);
+  console.log(z);
+}
+
+console.log(z);
